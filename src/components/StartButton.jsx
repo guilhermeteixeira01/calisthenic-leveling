19877734e51e20a77d8732b291ab10441e7276c7
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2025-12-28 18:40:31 -0300

    Atualiza referências de ícone e áudio para usar PUBLIC_URL

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,11 +1,12 @@
 export default function StartButton({ onClick }) {
     const handleClick = () => {
-
-        const audio = new Audio("/sounds/Jinwoo vs Igris The Bloodred.mp3");
-        audio.loop = true;         // tocar continuamente
+        // Cria o objeto de áudio usando PUBLIC_URL
+        const audio = new Audio(`${process.env.PUBLIC_URL}/sounds/Jinwoo vs Igris The Bloodred.mp3`);
+        audio.loop = true;        // tocar continuamente
         audio.volume = 0.3;
-        audio.play();
-        onClick();
+        audio.play().catch((err) => {
+            console.log("Erro ao reproduzir áudio:", err);
+        });
 
         // Executa a função de start
         onClick();
