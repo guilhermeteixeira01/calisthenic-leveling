bf0db8330c81e1018b8e4da71fcc81f794cad625
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-02-05 10:16:09 -0300

    Update App Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teixeira.calisthenicleveling',
+  appName: 'Calisthenic Leveling',
+  webDir: 'build',
+};
+
+export default config;
