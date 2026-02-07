fcc6e5332d12b51f11ba52c2760b1cd1c0843c5b
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-02-07 11:30:11 -0300

    remove android modules

--- android/app/src/main/java/com/teixeira/calisthenicleveling/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.teixeira.calisthenicleveling;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.teixeira.calisthenicleveling',
-  appName: 'Calisthenic Leveling',
-  webDir: 'build',
-};
-
-export default config;
