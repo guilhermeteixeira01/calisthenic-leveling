815f716e1357dd67ab56335a65b8ff907a28e2a8
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-02-08 16:48:35 -0300

    Update Fix

--- android/app/src/main/java/com/calisthenicleveling/app/MainActivity.java
+++ android/app/src/main/java/com/calisthenicleveling/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.calisthenicleveling.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            webView.clearCache(true);
+            webView.clearHistory();
+        }
+    }
+}
