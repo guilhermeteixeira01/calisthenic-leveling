32fcd45099231d2bba29c89aff00a8b922b8c91a
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-02-11 09:38:14 -0300

    Update XP por Rank

--- src/constants/xpPorRank.js
+++ src/constants/xpPorRank.js
@@ -3,12 +3,12 @@ export const LEVELMAX = 100;
 export const XP_POR_PONTO = 200;
 
 export const XP_POR_RANK = {
-    E: 150,
-    D: 300,
-    C: 500,
-    B: 1100,
-    A: 2500,
-    S: 3000,
+    E: 300,    // 3 semanas ≈ 0 meses
+    D: 1000,   // 10 semanas ≈ 2 meses e 2 semanas
+    C: 2500,   // 25 semanas ≈ 5 meses e 3 semanas
+    B: 6000,   // 60 semanas ≈ 1 ano e 8 semanas (≈ 1 ano e 2 meses)
+    A: 12000,  // 120 semanas ≈ 2 anos e 16 semanas (≈ 2 anos e 4 meses)
+    S: 20000,  // 200 semanas ≈ 3 anos e 44 semanas (≈ 3 anos e 10 meses)
 };
 
 export const RANKS = [
